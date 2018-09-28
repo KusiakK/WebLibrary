e21ed384ce4245a92e895701d0582f590ebc9950
KusiakK <kusiak.konrad@gmail.com>
2018-09-28 19:44:15 +0200

    repositories updated

--- src/main/java/pl/kusiakk/weblibrary/repositories/AuthorRepository.java
+++ src/main/java/pl/kusiakk/weblibrary/repositories/AuthorRepository.java
@@ -3,8 +3,5 @@ package pl.kusiakk.weblibrary.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.kusiakk.weblibrary.domain.models.Author;
 
-import java.util.List;
-
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
-    List<Author> findAll();
 }
