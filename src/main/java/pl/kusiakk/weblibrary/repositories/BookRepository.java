d8b99aa302d632f24bf74e7e7402f9944ff2fbbe
blysku <blyskx@gmail.com>
2018-09-28 19:44:15 +0200

    repositories updated

--- src/main/java/pl/kusiakk/weblibrary/repositories/BookRepository.java
+++ src/main/java/pl/kusiakk/weblibrary/repositories/BookRepository.java
@@ -3,8 +3,5 @@ package pl.kusiakk.weblibrary.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.kusiakk.weblibrary.domain.models.Book;
 
-import java.util.List;
-
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    List<Book> findAll();
 }
