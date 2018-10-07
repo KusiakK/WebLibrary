7c6bfa4372239218e1f93f905ea6d70166c66468
KusiakK <kusiak.konrad@gmail.com>
2018-10-07 22:08:23 +0200

    testing book controller

--- src/test/java/pl/kusiakk/weblibrary/controllers/BookControllerTest.java
+++ src/test/java/pl/kusiakk/weblibrary/controllers/BookControllerTest.java
@@ -6,14 +6,22 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import pl.kusiakk.weblibrary.domain.exceptions.AuthorNotFoundException;
+import pl.kusiakk.weblibrary.domain.exceptions.BookNotFoundException;
+import pl.kusiakk.weblibrary.domain.models.Author;
+import pl.kusiakk.weblibrary.domain.models.Book;
 import pl.kusiakk.weblibrary.repositories.BookRepository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static pl.kusiakk.weblibrary.controllers.AuthorControllerTest.asJsonString;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -46,11 +54,31 @@ public class BookControllerTest {
 
     @Test
     public void testIfReturnsCorrectSingleBook() throws Exception {
-        int tolkienId = 1;
-        mockMvc.perform(get("/api/v1/books/{id}", tolkienId))
+        int fellowshipId = 1;
+        mockMvc.perform(get("/api/v1/books/{id}", fellowshipId))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.isbn").value(9780007488315L));
     }
+    @Test
+    public void testIfPutModifyExistingBook() throws Exception {
+        Book book = new Book();
+        book.setIsbn("9780007488319");
+        Book testBook = new Book();
+        int fellowshipId = 1;
+
+        mockMvc.perform(put("/api/v1/books/{id}", fellowshipId)
+                .content(asJsonString(book))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        testBook = repository.findById(fellowshipId).orElseThrow(BookNotFoundException::new);
+
+        assertThat(testBook.getIsbn()).isEqualTo(book.getIsbn());
+    }
+
+
+
 
 }
